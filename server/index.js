2e06f1e3b5f037b9a9b3529605bbb3820f00783a
stevenhao <stevenhao@gmail.com>
2018-08-25 14:20:46 -0700

    dump everything

---
+++ server/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const path = require('path');
+const Topcoder = require('./topcoder');
+const redis = require("redis");
+const expressRedisCache = require('express-redis-cache');
+require('dotenv').config()
+
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
+console.log(process.env.tcsso);
+const server = express();
+const topcoder = new Topcoder(process.env.tcsso);
+topcoder.initializeSocket();
+
+// Priority serve any static files.
+server.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+
+// cache things
+const redisClient = redis.createClient(process.env.REDIS_URI || 'redis://127.0.0.1:6379');
+const cache = expressRedisCache({
+  client: redisClient,
+  expire: 60 * 60,
+});
+server.use(cache.route());
+
+// Answer API requests.
+server.get('/api/user/:user', function (req, res) {
+  const user = req.params.user;
+  res.set('Content-Type', 'application/json');
+  topcoder.getUser(user)
+    .then(user => {
+      res.json(user);
+    });
+});
+
+server.get('/api/round/:round', function (req, res) {
+  const round = req.params.round;
+  res.set('Content-Type', 'application/json');
+  topcoder.getRound(round)
+    .then(coders => {
+      res.json(coders);
+    });
+});
+
+// All remaining requests return the React app, so it can handle routing.
+server.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+});
+
+module.exports = server;
---
+++ server/topcoder.js
@@ -0,0 +1,75 @@
+const socketio = require('socket.io-client');
+const fetch = require('node-fetch');
+const Promise = require('bluebird');
+const _ = require('lodash');
+
+class Topcoder {
+  constructor(sso) {
+    this.sso = sso;
+  }
+
+  initializeSocket() {
+    console.log('connecting');
+    this.socket = socketio.connect('https://arenaws.topcoder.com');
+    this.socket.on('connect', () => {
+      console.log('connected!');
+      this.socket.emit('SSOLoginRequest', {
+        sso: this.sso,
+      });
+    });
+    this.socket.on('event', () => {
+      console.log('event!');
+    });
+    this.socket.on('disconnect', () => {
+      console.log('disconnected!');
+    });
+  }
+
+  getUser(username) {
+    return fetch(`https://api.topcoder.com/v3/members/${username}/stats/`)
+      .then(r => r.json())
+      .then(body => body.result.content.DATA_SCIENCE.SRM)
+      .then(({ rank: { rating, volatility, competitions } }) => ({
+        username,
+        rating,
+        volatility,
+        competitions,
+      }))
+      .catch(() => ({
+        username,
+        rating: 1200,
+        volatility: 500,
+        competitions: 0,
+      }));
+  }
+
+  getRound(roundID=17247, divisionID=1) {
+    console.log('emitting');
+    this.socket.emit('DivSummaryRequest', {
+      roundID,
+      divisionID,
+    });
+    return new Promise((resolve, reject) => {
+      const numRooms = 9;
+      let roomsReceived = 0;
+      const roomData = {};
+      this.socket.on('CreateChallengeTableResponse', message => {
+        roomsReceived += 1;
+        const { roomID: roomId, coders } = message;
+        console.log('received', roomId);
+        roomData[roomId] = {
+          coders,
+        };
+        if (roomsReceived === numRooms) {
+          resolve(roomData);
+        }
+      });
+    }).then(roomData => {
+      this.socket.removeAllListeners('CreateChallengeTableResponse');
+      const allCoders = _.flatten(_.values(roomData).map(({ coders }) => coders));
+      return allCoders;
+    });
+  }
+}
+
+module.exports = Topcoder;
