2ec729560a4375fae9bb6b22b762dad608faeee3
stevenhao <stevenhao@gmail.com>
2018-08-26 13:23:46 -0700

    restyle app theme

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -55,12 +55,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">2018 TCO Algorithm Wilcard Fun Round</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <RatingTable data={this.state.data} />
       </div>
     );
